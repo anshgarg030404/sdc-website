82619c34593d78724f50e0045db4eeaf494915a5
Shreya Shree <145593749+shreyagitt@users.noreply.github.com>
2024-08-17 07:13:56 +0000

    Font Modification

--- src/app/about-us/become-mem.tsx
+++ src/app/about-us/become-mem.tsx
@@ -1,9 +1,9 @@
 export default function Become(){
     return(
         <>
-            <div className="space-y-3 items-center">
+            <div className="flex flex-col space-y-3 items-center">
                         <h1 className="uppercase">become a member</h1>
-                        <button className="items-center uppercase bg-black border hover:bg-white hover:duration-500 hover:text-black delay-200 border-white px-10 py-2 md:text-3xl">join us</button>
+                        <button className="items-center uppercase bg-black border hover:bg-white hover:duration-500 hover:text-black delay-200 border-white px-10 py-2 md:text-3xl">JOIN US</button>
                     </div>
         </>
     )
